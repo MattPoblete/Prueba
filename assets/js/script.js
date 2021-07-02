7f92cd13868fe8549ea9bc8504b280855e75e373
Matias <matias.pobleteduran@gmail.com>
2021-07-01 21:40:58 -0400

    form funcional por fin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
-if ($('#contactForm').is('visible')) {
-  let a = $('#modal-form').height();
-console.log("a")
-}
+/*$('#contactForm').on('shown.bs.modal', function() {
+  console.log("1")
+  let a = $("#modal-form").height();
+  console.log(a)
+  $(".modal-content").height(a);
+  $("iframe").height(a);
+})
+ */
 
-
- 
+$('#contactForm').on('shown.bs.modal', function() { 
+  $(".modal-form").on("click", (function () { 
+    console.log("°")
+    $("#contactForm").hide();
+    
+  }))
+});
