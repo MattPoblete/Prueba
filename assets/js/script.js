91965de44172bf3d49efaf27e845ca7cb0de5628
Matias <matias.pobleteduran@gmail.com>
2021-07-02 03:46:03 -0400

    por fin pagina terminada, creo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,37 @@
-/*$('#contactForm').on('shown.bs.modal', function() {
-  console.log("1")
-  let a = $("#modal-form").height();
-  console.log(a)
-  $(".modal-content").height(a);
-  $("iframe").height(a);
-})
- */
-
-$('#contactForm').on('shown.bs.modal', function() { 
-  $(".modal-form").on("click", (function () { 
-    console.log("°")
-    $("#contactForm").hide();
-    
-  }))
+//asegurar carga de documento//
+$(document).ready(function(){
+  //Smooth scroll/
+  $("#navbar a").on("click", (function (evento) { 
+    evento.preventDefault();
+    let elemento = $(this).attr("href");
+  console.log(elemento);
+  $('html, body').animate({scrollTop: $(elemento).offset().top}, 1500);
+  }));
+//------//
+//Tootltips//
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+//------//
+//modals//
+  $('#btn-modal-myc').click(function(){
+    $('#modal-myc').modal('show')
+  })
+  $('#btn-modal-iguana').click(function(){
+    $('#modal-iguana').modal('show')
+  })
+  $('#btn-modal-suricata').click(function(){
+    $('#modal-suricata').modal('show')
+  })
+  $('#btn-modal-cuppon').click(function(){
+    $('#modal-cuppon').modal('show')
+  })
+  $('#btn-modal-ricomida').click(function(){
+    $('#modal-ricomida').modal('show')
+  })
+  $('#btn-modal-evaluacion').click(function(){
+    $('#modal-evaluacion').modal('show')
+  })
+//-----//
 });
+//Fin del Script//
